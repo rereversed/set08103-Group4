1bf8ce98b8e51fa27cfc96a0da51d8299548c112
Jinson Thayil <jinsonthayil@hotmail.co.uk>
2024-03-12 16:40:40 +0000

    Adding classes for required tables

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,33 @@
+package com.napier.sem;
+
+public class Country {
+    public String code;
+
+    public String name;
+
+    public String continent;
+
+    public String region;
+
+    public double surfaceArea;
+
+    public int IndepYear;
+
+    public int population;
+
+    public double lifeExpectancy;
+
+    public double GNP;
+
+    public double GNPOLD;
+
+    public String localName;
+
+    public String govermentFrom;
+
+    public String headOfState;
+
+    public int capital;
+
+    public String code2;
+}
---
+++ src/main/java/com/napier/sem/Countrylanguage.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+public class Countrylanguage
+{
+
+    public String counytryCode;
+
+    public String language;
+
+    public String isOfficial;
+
+    public double percentage;
+
+}
