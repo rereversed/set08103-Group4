95289873d08213f32f891d816829401dbdc781a0
Artur Pacut <arturpacut904@gmail.com>
2024-03-21 15:50:16 +0000

    feat:Display city function

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,7 +7,7 @@ public class App {
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
+    public static Connection con = null;
 
     /**
      * Connect to the MySQL database.
@@ -55,59 +55,64 @@ public class App {
         }
     }
 
-    public City getCity(int ID) {
-        try {
-            //Create SQL Statement
-            Statement stmt = con.createStatement();
+//    public City getCity(int ID) {
+//        try {
+//            //Create SQL Statement
+//            Statement stmt = con.createStatement();
+//
+//            //Create string for SQL statement
+//            String strSelect =
+//                    "SELECT ID, Name, CountryCode, District, Population "
+//                            + "FROM city "
+//                            + "WHERE ID='" + ID + "'";
+//            //Execute SQL statement
+//            ResultSet rset = stmt.executeQuery(strSelect);
+////            Return city data if valid
+//            //Check one is returned
+//            if (rset.next()) {
+//                City city = new City();
+//                city.ID = rset.getInt("city.ID");
+//                city.Name = rset.getString("city.Name");
+//                city.CountryCode = rset.getString("city.CountryCode");
+//                city.District = rset.getString("city.District");
+//                city.Population = rset.getInt("city.Population");
+//                return city;
+//            } else {
+//                return null;
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            System.out.println("Failed to get city details");
+//            return null;
+//        }
+//    }
+//
 
-            //Create string for SQL statement
-            String strSelect =
-                    "SELECT ID, Name, CountryCode, District, Population "
-                            + "FROM city "
-                            + "WHERE ID='" + ID + "'";
-            //Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            //Return city data if valid
-            //Check one is returned
-            if (rset.next()) {
-                City city = new City();
-                city.ID = rset.getInt("city.ID");
-                city.Name = rset.getString("city.Name");
-                city.CountryCode = rset.getString("city.CountryCode");
-                city.District = rset.getString("city.District");
-                city.Population = rset.getInt("city.Population");
-                return city;
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get city details");
-            return null;
-        }
-    }
-
-    public void displayCity(City city) {
-        if (city != null) {
-            System.out.println(
-                    city.ID + " "
-                            + city.Name + " "
-                            + city.CountryCode + " "
-                            + city.District + " "
-                            + "Population " + city.Population);
-        }
-    }
+//    public void displayCity(City city) {
+//        if (city == null) {
+//            System.out.println("No Cities");
+//        }
+//
+//        if (city != null) {
+//            System.out.println(
+//                    city.ID + " "
+//                            + city.Name + " "
+//                            + city.CountryCode + " "
+//                            + city.District + " "
+//                            + "Population " + city.Population);
+//        }
+//    }
 
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
-
         // Connect to database
         a.connect();
 
-        City city = a.getCity(1);
-        a.displayCity(city);
+        City.worldCities();
+
         // Disconnect from database
+
         a.disconnect();
     }
 
--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,15 +1,112 @@
 package com.napier.sem;
 
+import java.util.ArrayList;
+import java.sql.*;
 
 public class City {
-    public int ID;
+    //Initialising variables for City
+    private int ID;
 
-    public String Name;
+    //Get and Set methods for City ID.
+    public int getID() {
+        return ID;
+    }
 
-    public String CountryCode;
+    public void setID(int setID) {
+        this.ID = setID;
+    }
 
-    public String District;
+    private String Name;
 
-    public int Population;
+    //Get and Set methods for City
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String setName) {
+        this.Name = setName;
+    }
+
+    private String CountryCode;
+
+    //Get and Set methods for City Country Code
+    public String getCountryCode() {
+        return CountryCode;
+    }
+
+    public void setCountryCode(String setCountryCode) {
+        this.CountryCode = setCountryCode;
+    }
+
+    private String District;
+
+    //Get and Set methods for City District
+    public String getDistrict() {
+        return District;
+    }
+
+    public void setDistrict(String setDistrict) {
+        this.District = setDistrict;
+    }
+
+    private int Population;
+
+    //Get and Set methods for City Population
+    public int getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(int setPopulation) {
+        this.Population = setPopulation;
+    }
+
+
+    public static ArrayList<City> worldCities() {
+        String Select = "SELECT * FROM city ORDER BY population DESC";
+        return getCities(Select);
+    }
+
+    public static ArrayList<City> getCities(String strSelect) {
+        ArrayList<City> Cities = new ArrayList<>();
+        try {
+            //Create SQL Statement
+            Statement stmt = App.con.createStatement();
+            //Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            while (rset.next()) {
+                City city = new City();
+                city.setName(rset.getString("city.Name"));
+                city.setCountryCode(rset.getString("city.CountryCode"));
+                city.setDistrict(rset.getString("city.District"));
+                city.setPopulation(rset.getInt("city.Population"));
+
+                Cities.add(city);
+            }
+
+            //Return city data if valid
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get city details");
+            return null;
+
+        }
+        displayCities(Cities);
+        return Cities;
+    }
+
+
+    public static void displayCities(ArrayList<City> cities) {
+        for (int i = 0; i < cities.size(); i++) {
+            City city = cities.get(i);
+            System.out.println("Name " + city.getName());
+            System.out.println("Country Code " + city.getCountryCode());
+            System.out.println("District " + city.getDistrict());
+            System.out.println("Population " + city.getPopulation());
+            System.out.println("_______________________");
+
+        }
+
+    }
 }
 
