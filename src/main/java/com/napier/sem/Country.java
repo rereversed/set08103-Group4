59eef7f1354dc6bb6e64e0612a639cee1914efc0
Artur Pacut <arturpacut904@gmail.com>
2024-04-18 10:33:15 +0100

    feat: get region population

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -10,25 +10,23 @@ public class Country {
     public int population;
 
 
-    public void getCitiesByCountryPopulation(Connection con) {
+    public void getCitiesByCountryPopulation(Connection con, String country) {
         if (con == null) {
-            System.out.println("Connectioxn is null.");
+            System.out.println("Connection is null.");
             return;
         }
 
-        String query = "SELECT * FROM city ORDER BY population DESC";
+        String query = "SELECT * FROM city JOIN country ON city.CountryCode = country.Code WHERE country.name = '" + country + "' ORDER BY city.population DESC";
 
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
 
-                String code = rs.getString("CountryCode");
                 String name = rs.getString("name");
+                int population = rs.getInt("population");
 
-                System.out.println(name);
-                System.out.println("Country Code: " + code);
-                System.out.println("____________");
+                System.out.printf("- %s, Population: %d \n", name, population);
 
             }
         } catch (SQLException e) {
@@ -39,7 +37,7 @@ public class Country {
     }
 
     public void getCitiesByDistrictPopulation(Connection con, String name) {
-        if ( con == null){
+        if (con == null) {
             System.out.println("Connection is null");
             return;
         }
@@ -50,24 +48,23 @@ public class Country {
                 "ORDER BY Population DESC";
 
 
-
         try (Statement stmt = con.createStatement();
-             ResultSet rs = stmt.executeQuery(query)){
-            while (rs.next()){
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
                 String cityName = rs.getString("Name");
                 int population = rs.getInt("Population");
 
-                System.out.println(cityName + "  " +  population );
+                System.out.println(cityName + "  " + population);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get Population details: " + e.getMessage());
         }
 
     }
 
-    public void getTopNCitiesByCountryPopulation(Connection con, int n) {
+    public void getTopNCitiesByCountryPopulation(Connection con, int n, String country) {
         if (con == null) {
-            System.out.println("Connectioxn is null.");
+            System.out.println("Connection is null.");
             return;
         }
         if (n < 1) {
@@ -75,12 +72,15 @@ public class Country {
             n = 1;
         }
 
-        String query = "SELECT * FROM country ORDER BY population DESC LIMIT " + n;
+        String query = "SELECT * " +
+                "FROM city INNER JOIN country ON country.Code = city.CountryCode " +
+                " WHERE country.name = '" + country
+                + "' ORDER BY city.population DESC LIMIT " + n;
 
 
         try (Statement stmt = con.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
-            System.out.println("Top " + n + " Populated Countries:");
+            System.out.println("Top " + n + " Populated Cities in " + country + ":");
             while (rs.next()) {
                 String name = rs.getString("name");
                 int population = rs.getInt("population");
@@ -93,31 +93,31 @@ public class Country {
     }
 
     public void getTopNCitiesByDistrictPopulation(Connection con, int n, String name) {
-        if (con == null){
+        if (con == null) {
             System.out.println("Connection is Null");
             return;
         }
-        if (n<1){
+        if (n < 1) {
             System.out.println("N Must be at least 1");
-            n=1;
+            n = 1;
         }
 
         String query = "SELECT Name, Population " +
-                        "FROM city " +
-                        "WHERE district = '" + name + "'" +
-                        "ORDER By Population DESC " +
-                        "LIMIT " + n;
+                "FROM city " +
+                "WHERE district = '" + name + "'" +
+                "ORDER By Population DESC " +
+                "LIMIT " + n;
 
         try (Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(query)){
+             ResultSet rs = stmt.executeQuery(query)) {
             System.out.println(("Top " + n + " " + "Populated Countries:"));
 
-            while(rs.next()){
+            while (rs.next()) {
                 String cityName = rs.getString("name");
                 int population = rs.getInt("population");
                 System.out.printf("- %s, Population: %d \n", cityName, population);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get Required Details: " + e.getMessage());
         }
     }
@@ -127,35 +127,35 @@ public class Country {
         String countryCode = null;
         int totalPopulation = 0;
 
-        if (con == null){
+        if (con == null) {
             System.out.println("Connection is null");
             return;
         }
 
         String query = "SELECT * " +
-                        "FROM country " +
-                        "WHERE name = '" + name + "'";
+                "FROM country " +
+                "WHERE name = '" + name + "'";
 
         try (Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
-            if(rs.next()){
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
                 totalPopulation = rs.getInt("Population");
                 countryCode = rs.getString("code");
             }
-        }catch (SQLException e){
-            System.out.println("Failed to get Population Details: " + e.getMessage() );
+        } catch (SQLException e) {
+            System.out.println("Failed to get Population Details: " + e.getMessage());
         }
 
 
         String query2 = "SELECT SUM(population) AS total_population_in_cities " +
-                        "FROM city " +
-                        "WHERE countrycode = '" + countryCode + "'";
+                "FROM city " +
+                "WHERE countrycode = '" + countryCode + "'";
 
 
         try (Statement stmt2 = con.createStatement();
-            ResultSet rs = stmt2.executeQuery(query2)){
+             ResultSet rs = stmt2.executeQuery(query2)) {
 
-            if (rs.next()){
+            if (rs.next()) {
                 int populationInCities = rs.getInt("total_population_in_cities");
                 int populationNotInCities = (totalPopulation - populationInCities);
 
@@ -164,7 +164,7 @@ public class Country {
                 System.out.println("Population Not in Cities: " + populationNotInCities);
             }
 
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get Population Deatils: " + e.getMessage());
         }
     }
@@ -197,51 +197,51 @@ public class Country {
     }
 
     public void getDistrictPopulation(Connection con, String name) {
-        if(con == null){
+        if (con == null) {
             System.out.println("Connection is null");
             return;
         }
 
         String query = "SELECT SUM(Population) AS total_population " +
-                        "FROM city " +
-                        "WHERE District = '" + name + "'";
+                "FROM city " +
+                "WHERE District = '" + name + "'";
 
 
-        try ( Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
-            if (rs.next()){
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
                 int population = rs.getInt("total_population");
 
                 System.out.println(name);
                 System.out.println(population);
             }
-        }catch (SQLException e){
-            System.out.println("Failed to get Population Details:" +  e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Failed to get Population Details:" + e.getMessage());
         }
     }
 
     public void getCityPopulation(Connection con, String name) {
-           if(con == null) {
-               System.out.println("Connection is null.");
-               return;
-           }
-           String query = "SELECT * " +
-                            "FROM city " +
-                            "WHERE Name ='" + name +"'";
+        if (con == null) {
+            System.out.println("Connection is null.");
+            return;
+        }
+        String query = "SELECT * " +
+                "FROM city " +
+                "WHERE Name ='" + name + "'";
 
-           try(Statement stmt = con.createStatement();
-               ResultSet rs = stmt.executeQuery(query)){
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-               if(rs.next()) {
-                   String cityName = rs.getString("Name");
-                   int population = rs.getInt("Population");
+            if (rs.next()) {
+                String cityName = rs.getString("Name");
+                int population = rs.getInt("Population");
 
-                   System.out.println(cityName);
-                   System.out.println("Total Population " + population );
-               }
-        }catch (SQLException e){
-               System.out.println("Failed to get population details " + e.getMessage());
-           }
+                System.out.println(cityName);
+                System.out.println("Total Population " + population);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to get population details " + e.getMessage());
+        }
     }
 }
 
--- src/main/java/com/napier/sem/Region.java
+++ src/main/java/com/napier/sem/Region.java
@@ -6,86 +6,88 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Region {
+    int population;
+
     public void getCountriesByRegionPopulation(Connection con, String name) {
-        if(con ==null){
+        if (con == null) {
             System.out.println("Connection is Null");
             return;
         }
 
-        String query =  "SELECT Name AS country_name, Region, Population " +
-                        "FROM country " +
-                        "WHERE Region =  '" + name + "'" +
-                        "ORDER BY Population DESC;";
+        String query = "SELECT Name AS country_name, Region, Population " +
+                "FROM country " +
+                "WHERE Region =  '" + name + "'" +
+                "ORDER BY Population DESC;";
 
-        try(Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-            while (rs.next()){
+            while (rs.next()) {
                 String countryName = rs.getString("country_name");
                 String region = rs.getString("Region");
                 int population = rs.getInt("Population");
 
                 System.out.println("Country: " + countryName + ", Region: " + region + ", Population: " + population);
             }
-            }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get Details " + e.getMessage());
         }
 
     }
 
     public void getTopNCountriesByRegionPopulation(Connection con, int n, String name) {
-        if (con == null){
+        if (con == null) {
             System.out.println("Connection is Null");
             return;
         }
-        if(n<1){
+        if (n < 1) {
             System.out.println("N must be at least 1 ");
-            n=1;
+            n = 1;
         }
 
         String query = "SELECT Name AS country_name, Population " +
-                        "FROM country " +
-                        "WHERE Region = '" + name + "'" +
-                        "ORDER BY Population DESC " +
-                        "LIMIT " + n;
+                "FROM country " +
+                "WHERE Region = '" + name + "'" +
+                "ORDER BY Population DESC " +
+                "LIMIT " + n;
 
-        try(Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
             System.out.println("Top " + n + " Populated Countries:");
 
-            while ((rs.next())){
+            while ((rs.next())) {
                 String countryName = rs.getString("country_name");
                 int population = rs.getInt("Population");
 
                 System.out.println(countryName + "   " + population);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println("Failed to get details " + e.getMessage());
         }
     }
 
     public void getCitiesByRegionPopulation(Connection con, String name) {
-        if (con == null){
+        if (con == null) {
             System.out.println("Connection is Null ");
             return;
         }
 
-        String query =  "SELECT city.Name AS city_name, city.Population " +
-                        "FROM city " +
-                        "JOIN country ON city.CountryCode = country.Code " +
-                        "WHERE country.Region = '" + name + "'" +
-                        "ORDER BY city.Population DESC;";
+        String query = "SELECT city.Name AS city_name, city.Population " +
+                "FROM city " +
+                "JOIN country ON city.CountryCode = country.Code " +
+                "WHERE country.Region = '" + name + "'" +
+                "ORDER BY city.Population DESC;";
 
-        try(Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(query)){
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
-            while (rs.next()){
+            while (rs.next()) {
 
                 String cityName = rs.getString("city_name");
                 int population = rs.getInt("Population");
                 System.out.println(cityName + "   " + population);
             }
-        }catch (SQLException e ){
+        } catch (SQLException e) {
             System.out.println("Failed to get details " + e.getMessage());
         }
 
@@ -103,6 +105,27 @@ public class Region {
     public void getPopulationDistributionByRegion(Connection con) {
     }
 
-    public void getRegionPopulation(Connection con) {
+    public void getRegionPopulation(Connection con, String region) {
+        if (con == null) {
+            System.out.println("Connection is Null");
+            return;
+        }
+
+
+        String query = "SELECT SUM(country.Population) AS totalPopulation FROM country WHERE country.Region = '" + region + "'";
+
+        try (Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            System.out.println("The region population is: ");
+
+            while ((rs.next())) {
+
+                int population = rs.getInt("totalPopulation");
+
+                System.out.println(region + "   " + population);
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to get details " + e.getMessage());
+        }
     }
 }
